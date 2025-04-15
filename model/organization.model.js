4352cd2cf5e1cadb2d251da6650e361c7827cc42
aatif13698 <aatif13698@gmail.com>
2025-04-15 16:26:08 +0530

    new changes for crash

---
+++ model/organization.model.js
@@ -0,0 +1,37 @@
+
+
+require('dotenv').config();
+
+
+const IS_DEV = process.env.NODE_ENV === 'development';
+const BASE_DOMAIN = IS_DEV ? 'localhost' : 'millionsform.com';
+const BASE_PORT = process.env.PORT || 3000;
+
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const ObjectId = Schema.ObjectId;
+
+const organizationSchema = new Schema(
+    {
+        userId: { type: ObjectId, ref: "User", index: true },
+        serialNumber: { type: String, default: null },
+        name: { type: String, required: true },
+        captionText: { type: String, default: null },
+        address: { type: String, default: null },
+        email: {
+            type: String, unique: true, lowecase: true,
+            trim: true, sparse: true, index: true
+        },
+        phone: { type: String, unique: true, sparse: true, trim: true, index: true }, 
+        logo: { type: String, default: null },
+        banner: { type: String, default: null },
+        isActive: { type: Boolean, default: false },
+        deletedAt: { type: Date, default: null, index: true }, // Index for soft-delete functionality
+    },
+    { timestamps: true }
+);
+
+
+const organizationModel = mongoose.model("organization", organizationSchema);
+module.exports = organizationModel;
--- utils/commonFunction.js
+++ utils/commonFunction.js
@@ -364,8 +364,10 @@ const restrictOtherCompany = async (req, res, next) => {
       const user = await User.findOne(query)
         .populate("role")
         .select("_id email phone");
+        console.log("user",user);
+        
       const findUserAccess = access.users.filter((item) => {
-        return item?._id?.toString() === user._id.toString();
+        return item?._id?.toString() === user?._id.toString();
       });
       if (findUserAccess?.length == 0) {
         return res.status(403).json({ message: "User does not have access to this company" });
